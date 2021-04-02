61f261a2c37bfd574a81ab9431cfed7a03edaf36
Jishnu Sardar <jishnu.sardar@bjitgroup.com>
2021-04-02 23:44:22 +0600

    Port issue fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-//require('dotenv').config()
+require('dotenv').config()
 //console.log(process.env.DB_USER);
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -10,8 +10,8 @@ const port = 5000;
 app.use(bodyParser.json());
 app.use(cors());
 const MongoClient = require('mongodb').MongoClient;
-//const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ayo4r.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const uri = "mongodb+srv://emaWatson:emawatson71@cluster0.ayo4r.mongodb.net/emaJohnStore?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ayo4r.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+//const uri = "mongodb+srv://emaWatson:emawatson71@cluster0.ayo4r.mongodb.net/emaJohnStore?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db("emaJohnStore").collection("products");
@@ -61,4 +61,4 @@ client.connect(err => {
 });
 
 
-app.listen(port)
+app.listen(process.env.PORT || port)
